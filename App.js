cef321769e0f67ff71799d39962b7b0359172be1
sokkun <sokkun.norn2019@gmail.com>
2021-05-04 20:35:32 +0700

    add screen login

--- App.js
+++ App.js
@@ -7,48 +7,50 @@ import {
   View
 } from 'react-native'
 
+import Login from './screen/Login'
 class App extends Component {
   render () {
     return (
-      <SafeAreaView>
-        <View
-          style={styles.container}
-        >
-          <ScrollView horizontal>
-            <View style={styles.box}>
-              <Text style={styles.text}>1</Text>
-            </View>
-            <View style={styles.box}>
-              <Text style={styles.text}>2</Text>
-            </View>
-            <View style={styles.box}>
-              <Text style={styles.text}>3</Text>
-            </View>
-          </ScrollView>
-        </View>
-      </SafeAreaView>
+      <Login />
+      // <SafeAreaView>
+      //   <View
+      //     style={styles.container}
+      //   >
+      //     <ScrollView horizontal>
+      //       <View style={styles.box}>
+      //         <Text style={styles.text}>1</Text>
+      //       </View>
+      //       <View style={styles.box}>
+      //         <Text style={styles.text}>2</Text>
+      //       </View>
+      //       <View style={styles.box}>
+      //         <Text style={styles.text}>3</Text>
+      //       </View>
+      //     </ScrollView>
+      //   </View>
+      // </SafeAreaView>
     )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    height: 400,
-    flexDirection: 'row',
-    backgroundColor: '#ccc'
-  },
-  box: {
-    width: 100,
-    height: 100,
-    borderRadius: 10,
-    borderColor: '#000',
-    borderWidth: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 20
-  }
-})
+// const styles = StyleSheet.create({
+//   container: {
+//     height: 400,
+//     flexDirection: 'row',
+//     backgroundColor: '#ccc'
+//   },
+//   box: {
+//     width: 100,
+//     height: 100,
+//     borderRadius: 10,
+//     borderColor: '#000',
+//     borderWidth: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+//   text: {
+//     fontSize: 20
+//   }
+// })
 
 export default App
